ffd5bc34a12218bb9df2b7b2ded284dadaa82dad
AndreaTerenz <andrea.terenziani.at@gmail.com>
2023-02-14 18:21:10 +0100

    hope?

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask
 from flask_sock import Sock
 
 app = Flask(__name__)
+sock = Sock(app)
 
 @app.route('/')
 def hello_world():
     print("calogero")
     return 'Hello, World!'
+
+@sock.route('/ws')
+def echo(ws):
+    while True:
+        data = ws.receive()
+        print(data)
+        ws.send(data)
